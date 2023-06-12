9d80d1a84dbaf167c307491ee63aa49b06b20be0
adiyakoby <adi.yakoby@gmail.com>
2023-06-12 13:58:20 +0300

    added birds spritesheet and staticocbjers spritesheet and started changes the game resources according to new use

--- include/Macro.h
+++ include/Macro.h
@@ -4,7 +4,7 @@
 //Const variables for window size:
 
 //original is 1400 800
-const unsigned int WINDOW_WIDTH = 1400;
-const unsigned int WINDOW_HEIGHT = 800;
+const unsigned int WINDOW_WIDTH = 1000;
+const unsigned int WINDOW_HEIGHT = 600;
 
 const float SCALE = 100.0f; // Scale factor between Box2D and SFML
