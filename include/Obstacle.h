0062e6bd69895f4e98d76f4bbb1d770a9a87fbed
adiyakoby <adi.yakoby@gmail.com>
2023-06-22 12:18:34 +0300

    added obstacle .h and .cpp

---
+++ include/Obstacle.h
@@ -0,0 +1,25 @@
+#pragma once
+
+
+#include "StaticObjects.h"
+
+class Obstacle :public StaticObjects
+{
+public:
+	Obstacle(std::shared_ptr<World> world, const sf::Vector2f position, const sf::Vector2f& size);
+	virtual ~Obstacle() = default;
+
+	void objectUpdate() override;
+	void drawObject(sf::RenderWindow& window) override;
+	sf::Vector2f getPosition() const override { return m_wood.getPosition(); }
+	void rotate(const int& x) override { this->rotateBody(); };
+
+	void hitState() override;
+
+private:
+	sf::RectangleShape m_Obstacle;
+	bool m_hit;
+	void initPhysicBody(std::shared_ptr<World> world, const sf::Vector2f& postion, const sf::Vector2f& size)override;
+	void initGraphicBody(const sf::Vector2f& size)override;
+};
+
